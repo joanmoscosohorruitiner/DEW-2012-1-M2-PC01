fabaf054118fb0c5ea02b3b2fc2407508c1d5d8a
joanmoscosohorruitiner <joanmay607@hotmail.com>
2012-07-14 12:41:05 -0500

    sdfds

---
+++ tienda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienda::Application.config.secret_token = '56ec72003fe2be411a46831fa1018efec2f7d444b11f250d25fe407dad93ef37685f3d43db7c173b99c02356476a711bca9457e67a720a88f1bcbe08f39a1a65'
